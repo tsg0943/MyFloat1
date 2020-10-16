f82a7469bef5677101bb1b7893d9ac988d135548
Ttsrr <tanya.grishina.02@gmail.com>
2020-10-16 23:33:49 +0300

    class MyFloat

--- MyFloat/Main.java
+++ MyFloat/Main.java
@@ -1,11 +1,25 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Collections;
 
 public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("2");
+        list.add("3");
+        list.add("1");
+        list.add("7");
+        list.add("6");
+        System.out.println(list);
+        Collections.swap(list, 0, 1);
+        System.out.println(list);
+        Collections.sort(list);
+        for (String str : list) {
+            System.out.println(" " + str);
 
-    public static void main(String[] args)
-    {
-        MyFloat a = new MyFloat(23456, 2);
-        MyFloat b = new MyFloat(86487961, 7);
-        a.diff(b);
+        }
     }
 }
+
