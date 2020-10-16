ab5552097d3e9c213711f8c449fd19d616bdd13d
Ttsrr <tanya.grishina.02@gmail.com>
2020-10-16 23:11:52 +0300

    public class MyFloat

---
+++ MyFloat/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        MyFloat a = new MyFloat(23456, 2);
+        MyFloat b = new MyFloat(86487961, 7);
+        a.diff(b);
+    }
+}
